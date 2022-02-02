98295acfd5695ff2517952c6722fd82a503c587c
Ostrowski Stanislaw EXT FRD SpyroSoft <stanislaw.ostrowski.spyrosoft@zf.com>
2022-02-02 16:51:00 +0100

    docker start script

---
+++ maintenance_workspace/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+K3S_GITOPS_ROOT_DIR="$(dirname "$SCRIPTPATH")"
+
+docker run --rm -it \
+    -v ${K3S_GITOPS_ROOT_DIR}:/repo \
+    -w="/repo" \
+    k3s_maintenance:latest \
+    bash
