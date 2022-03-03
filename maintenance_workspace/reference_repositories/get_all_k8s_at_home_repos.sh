484ed6739b435c38de00001369dca60691d080bf
Stanislaw Ostrowski <ostanislaw@gmail.com>
2022-03-03 13:59:22 +0000

    script for cloning all awesome k8s reference repositories

---
+++ maintenance_workspace/reference_repositories/get_all_k8s_at_home_repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -s https://github.com/k8s-at-home/awesome-home-kubernetes | grep href=\"https://github.com | sed -n "s/<td><a href=\"//p"  | sed 's/\">.*//g' | grep --invert-match 'p3lim/rudder' > repo_list.txt
+
+while read p; do
+  md5=$(echo $p | md5sum | cut -d ' ' -f 1)
+  #echo "$md5"
+  print "cloning $p into $md5"
+  git clone $p $md5
+done <repo_list.txt
+
+#| xargs -L 1 git clone
